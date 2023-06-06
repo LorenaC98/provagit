b3002c019a19d9b3625b850021af5f3b44d9597b
Lorena <lorenacus98@gmail.com>
2023-06-06 18:41:55 +0200

    profilo nuovo studente

---
+++ ESTeam.md/esercizio1.js
@@ -0,0 +1,12 @@
+let student {
+- name = "Alice"
+- surname = "Rossi"
+- age = 23
+- city = "Trapani"
+- hobby = "Fotografia"
+- favoriteFood = "Pizza"
+- favoriteVideoGame = "LoL"
+- favoriteFilm = "Sniper"
+- favoriteBook = "Resto qui"
+- petName = "Macchia"
+}
